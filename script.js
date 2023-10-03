7c31bb49cd2f7ea295efab0900c7af32d929cbbd
voidsoul-host <sanskar.purohit946@gmail.com>
2023-10-03 22:51:31 +0530

    name changed & link also changed

--- script.js
+++ script.js
@@ -23,6 +23,6 @@ ipApi();
 // async function ipData()
 // https://voidsoul-host.github.io/youtube-clone/
 searchbtn.addEventListener('click', ()=>{
-    document.cookie= `ipAddress=${ipAddress}; path=https://voidsoul-host.github.io/post-office-app/Result.html`;
-    window.location.href="https://voidsoul-host.github.io/post-office-app/Result.html"
+    document.cookie= `ipAddress=${ipAddress}; path=https://voidsoul-host.github.io/post-office-app/result.html`;
+    window.location.href="https://voidsoul-host.github.io/post-office-app/result.html"
 });
