c16e5051bdd23fc1fac30aabbd2ba25cf6b42e35
voidsoul-host <sanskar.purohit946@gmail.com>
2023-10-04 22:57:32 +0530

    new update 3.0

--- script.js
+++ script.js
@@ -23,6 +23,7 @@ ipApi();
 // async function ipData()
 // https://voidsoul-host.github.io/youtube-clone/
 searchbtn.addEventListener('click', ()=>{
-    document.cookie= `ipAddress=${ipAddress}; path=https://voidsoul-host.github.io/post-office-app/result.html`;
+    // document.cookie= `ipAddress=${ipAddress}; path=https://voidsoul-host.github.io/post-office-app/result.html`;
+    localStorage.setItem('ipAddress',`${ipAddress}`);
     window.location.href="https://voidsoul-host.github.io/post-office-app/result.html"
 });
